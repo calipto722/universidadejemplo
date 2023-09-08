a02216bc78cbb8773d769c8ad08a134d231fc100
calipto722 <141640050+calipto722@users.noreply.github.com>
2023-09-08 11:54:46 -0300

    Materiadata

--- src/universidadejemplo/AccesoADatos/MateriaData.java
+++ src/universidadejemplo/AccesoADatos/MateriaData.java
@@ -5,10 +5,33 @@
  */
 package universidadejemplo.AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import universidadejemplo.Entidades.Materia;
 /**
  *
  * @author noelia
  */
 public class MateriaData {
     
+    private Connection con =null;
+    
+    public MateriaData(){
+     con =Conexion.getConexion();
+}
+public void guargarMateria (Materia materia){
+ String sql= "INSERT INTO `materia`( `nombre`, `año`, `estado`) VALUES ('?','?','?','?')";
+
+        try {
+            PreparedStatement ps= con.prepareStatement(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+
+}
 }
