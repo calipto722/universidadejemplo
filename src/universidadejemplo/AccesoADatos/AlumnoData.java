1cc47adb80d47e4e6fb632200a0965f872d6ad3f
LoD-Marksherry <elduomelli@gmail.com>
2023-09-08 12:20:15 -0300

    terminacion AlumnoData
    Chequear alumnoData por las dudas. pero ya tiene todos sus metodos.

--- src/universidadejemplo/AccesoADatos/AlumnoData.java
+++ src/universidadejemplo/AccesoADatos/AlumnoData.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import universidadejemplo.Entidades.Alumno;
 
@@ -145,6 +143,52 @@ public class AlumnoData {
        return alumnos;
    }
    
+   public void modificarAlumno (Alumno alumno){
+       
+       String sql= "UPDATE alumno SET dni=? , apellido= ?, nombre=?, fechaNacimiento= ? WHERE idAlumno= ? ";
+       PreparedStatement ps=null;
+       
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setInt(1,alumno.getDni());
+            ps.setString(2,alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setInt(5, alumno.getIdAlumno());
+            int exito = ps.executeUpdate();
+            
+            if (exito==1){
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente. ");
+            }else {
+                JOptionPane.showMessageDialog(null, "El alumno no existe");
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al acceder a la tabla Alumno"+ ex.getMessage());
+            
+        }
+       
+               
+       
+   }
    
+   public void eliminarAlumno (int id){
+       
+        try {
+            String sql= "UPDATE alumno SET estado= 0 WHERE idAlumno= ? ";
+            PreparedStatement ps= con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila= ps.executeUpdate();
+            
+            if (fila==1){
+                JOptionPane.showMessageDialog(null, "Se elimino el alumno, ");
+            
+            }
+            ps.close();
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno");
+        }
+       
+   }
    
 }
